1e3f780ef860e1dbdf5ed3d6eb69ad2bcaf6eebb
Mirko van Ede <mvanede@bol.com>
2018-04-18 11:25:55 +0200

    Vulnerable dependency updated; made packageFile path mandatory

--- index.js
+++ index.js
@@ -1,57 +1,71 @@
-var fs = require('fs'),
-    _ = require('underscore'),
-    ejs = require('ejs');
+"use strict";
+
+const fs = require('fs');
+const _ = require('underscore');
+const ejs = require('ejs');
+const chalk = require('chalk');
 
 function VersionFile(options) {
-    var self = this;
-	
-    var defaultOptions = {
-        outputFile: './version.txt',
-        template: 'version.ejs',
-        templateString: '',
-		packageFile: './package.json',
-        extras: {}
-    };
-	
-    //set default config data
-    var optionsObject = options || {};
-	self.options = _.defaults(optionsObject, defaultOptions);
-	self.options['package'] = require(self.options.packageFile);
+  var self = this;
+
+  var defaultOptions = {
+    outputFile: 'version.txt',
+    template: 'version.ejs',
+    templateString: '',
+    extras: {}
+  };
+
+  //Set default config data
+  var optionsObject = options || {};
+  self.options = _.defaults(optionsObject, defaultOptions);
+  
+  // Check for missing arguments
+  if (!this.options.packageFile) {
+    throw new Error(chalk.red('Expected path to packageFile'))
+  }
+  
+  try {
+    self.options['package'] = require(self.options.packageFile);
+  } catch (err) {
+    throw new Error(chalk.red(err))
+  }
+
 }
-	
-VersionFile.prototype.apply = function(){
-	var self = this;
-	
-	
-    self.options.currentTime = new Date();
 
-    /*
-	 * If we are given a template string in the config, then use it directly.
-     * But if we get a file path, fetch the content then use it.
-	 */
-    if (self.options.templateString){
-        self.writeFile(self.options.templateString);
-    } else {
-        fs.readFile(self.options.template, {encoding: 'utf8'}, function(error, content){
+VersionFile.prototype.apply = function() {
+  var self = this;
+  self.options.currentTime = new Date();
 
-            if(error){
-                throw error;
-                return;
-            }
+  /*
+   * If we are given a template string in the config, then use it directly.
+   * But if we get a file path, fetch the content then use it.
+   */
+  if (self.options.templateString) {
+    self.writeFile(self.options.templateString);
+  } else {
+    fs.readFile(self.options.template, {
+      encoding: 'utf8'
+    }, function(error, content) {
+      if (error) {
+        throw error;
+        return;
+      }
 
-            self.writeFile(content);
-        });
-    }
+      self.writeFile(content);
+    });
+  }
 };
 
 /**
  * Renders the template and writes the version file to the file system.
  * @param templateContent
  */
-VersionFile.prototype.writeFile = function(templateContent){
-	var self = this;
-	fileContent = ejs.render(templateContent, self.options);
-    fs.writeFile(self.options.outputFile, fileContent, {flag: 'w'});
+VersionFile.prototype.writeFile = function(templateContent) {
+  var self = this;
+  fileContent = ejs.render(templateContent, self.options);
+  fs.writeFile(self.options.outputFile, fileContent, {
+    flag: 'w'
+  });
 }
 
 module.exports = VersionFile;
